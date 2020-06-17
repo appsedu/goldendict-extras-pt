6752b69f7ca74b3738931bbb7e8c5115addf374f
ailtonbsj <ailton.ifce@gmail.com>
2020-06-17 10:43:30 -0300

    fixed launcher

--- compile.sh
+++ compile.sh
@@ -1,8 +1,14 @@
+
 #!/bin/bash
 
 if [ ! -f dicts.tar.gz ]; then
-	wget "https://drive.google.com/uc?id=1yvLdTN0ZQYqV55lUn-7fGXn6s-NQZAfq&export=download" -O dicts.tar.gz
+	# wget "https://drive.google.com/uc?id=1yvLdTN0ZQYqV55lUn-7fGXn6s-NQZAfq&export=download" -O dicts.tar.gz
+	echo "You have to download the package and save as 'dicts.tar.gz'"
+	echo "URL:"
+	echo "https://drive.google.com/uc?id=1yvLdTN0ZQYqV55lUn-7fGXn6s-NQZAfq&export=download"
+	exit
 fi
 
+mkdir -p deb_package/usr/share/goldendict/dicts/
 tar -vxzf dicts.tar.gz -C deb_package/usr/share/goldendict/dicts/
 dpkg-deb -b deb_package .
